4b99d15c05d48eef4b4899078424bd41397313a4
A10Harsh <harshagarwal8002@gmail.com>
2025-06-17 22:38:28 +0530

    Added bubble sort

---
+++ sorting/bubble.java
@@ -0,0 +1,22 @@
+package sorting;
+
+public class bubble {
+    public static void bubbleShort(int arr[]){
+        for (int i = 0; i< arr.length-1; i++){
+
+            for (int j = 0; j<arr.length-1-i; j++){
+                if (arr[j]>arr[j+1]){
+                //swap 
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;    }}
+            
+        }
+     for (int i = 0; i< arr.length; i++){
+        System.out.print(arr[i]); 
+    }}
+    public static void main(String[] args) {
+        int [] number = {6,1,2,4,3,5,};
+        bubbleShort(number);
+}
+}
