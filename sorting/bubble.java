e2be6673530e8d324db2b8402063f59b0b0515b9
A10Harsh <harshagarwal8002@gmail.com>
2025-06-17 22:40:40 +0530

    Added string

--- sorting/bubble.java
+++ sorting/bubble.java
@@ -13,7 +13,7 @@ public class bubble {
             
         }
      for (int i = 0; i< arr.length; i++){
-        System.out.print(arr[i]); 
+        System.out.print("sorted"+arr[i]); 
     }}
     public static void main(String[] args) {
         int [] number = {6,1,2,4,3,5,};
