3babde7562810e818fb8c536fab8d3732247e41f
A10Harsh <harshagarwal8002@gmail.com>
2025-06-24 09:36:57 +0530

    find out the key in  matrices

---
+++ Array/keyMatrics.java
@@ -0,0 +1,46 @@
+// public class keyMatrics {
+
+//     public static int max(int matrix[][] ){
+//         int max = Integer.MIN_VALUE;
+//         for (int i= 0; i<matrix.length; i ++ ){
+//             for (int j = 0; j <matrix[0].length; j++){
+//                 if (max<matrix[i][j]){
+//                     max = matrix[i][j];
+//                 }
+                
+//             }
+//         }
+//         return max;
+//     }
+
+//     public 
+
+
+//      public static void key(int matrix[][],int key){
+     
+//         for (int i= 0; i<matrix.length; i ++ ){         
+//             for (int j = 0; j <matrix[0].length; j++){
+//                 if (key == matrix[i][j]){
+//                     System.out.printf("key is present  at index %d , %d %n", i, j);
+//                 }
+                
+//             }
+//         }
+       
+//     }
+
+//     public static void main(String[] args) {
+//         int [][] matrix = { {10,20,30,40},
+//                             {15,25,35,45},
+//                             {27,29,37,48},
+//                             {32,33,39,50}}; 
+    
+//     int max = max(matrix);
+    
+//     key(matrix, max);
+
+  
+    
+    
+//     }
+// }
