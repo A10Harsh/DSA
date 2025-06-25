faf69b048333f1ab480784299999f1a54ed25233
A10Harsh <harshagarwal8002@gmail.com>
2025-06-25 10:11:54 +0530

    pattern of printing the butterfly in console

--- pattern/butterfly.java
+++ pattern/butterfly.java
@@ -3,7 +3,6 @@ package pattern;
 public class butterfly {
     public static void main(String[] args) {
         int n = 5;
-        int x = 0;
         for (int i = 1; i<= n; i++){
            for(int j =1; j<=i;  j++){
             System.out.print("x");
