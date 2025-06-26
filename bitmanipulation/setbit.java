4a78a806bd6b028e215ba88e8408c6aa79d15994
A10Harsh <harshagarwal8002@gmail.com>
2025-06-26 10:37:39 +0530

    function of set bit

---
+++ bitmanipulation/setbit.java
@@ -0,0 +1,11 @@
+package bitmanipulation;
+
+public class setbit {
+    public static int set(int n, int i ){
+        int bitMask = 1<<i;
+        return n | bitMask;
+    }
+    public static void main(String[] args) {
+        System.out.println(set(10, 2));
+    }
+}
