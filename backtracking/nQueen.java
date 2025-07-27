b8d7dd05874676a3a585dd3a794829c73fca176d
A10Harsh <harshagarwal8002@gmail.com>
2025-07-27 08:47:49 +0530

    removes the small error

--- backtracking/nQueen.java
+++ backtracking/nQueen.java
@@ -28,7 +28,7 @@ public class nQueen {
                 return false;
             }
         }
-         for (int i = row -1, j = col+1; i>= 0 && j <=board.length; i--,j++){
+         for (int i = row -1, j = col+1; i>= 0 && j <board.length; i--,j++){
              if (board [i][j]== 'Q'){
                 return false;
             }
@@ -40,7 +40,7 @@ public class nQueen {
         System.out.println("------------board----------------");
           for (int i = 0; i<board.length; i++){
             for (int j =0; j<board.length; j++){
-               System.out.print(board[i][j]);
+               System.out.print(board[i][j] +" ");
             }
             System.out.println();
 
